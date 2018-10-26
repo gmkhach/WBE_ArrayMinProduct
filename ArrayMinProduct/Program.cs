df160db0cd116a197cecd4cfc6a31f6298487665
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-26 00:35:26 -0400

    brute-force method

--- ArrayMinProduct/Program.cs
+++ ArrayMinProduct/Program.cs
@@ -4,12 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayMinProduct
+namespace ArrayMaxProduct
 {
     class Program
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nInput an array of integer\n\n>>> ");
+                    string[] input = Console.ReadLine().Split(',');
+                    int[] intArr = new int[input.Length];
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        intArr[i] = Convert.ToInt32(input[i]);
+                    }
+                    Console.WriteLine($"\nMaxProduct: {MinProduct(intArr)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n{ex.Message}");
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static int MinProduct(int[] arr)
+        {
+            int min1 = arr[0] < arr[1] ? arr[0] : arr[1];
+            int min2 = arr[0] < arr[1] ? arr[1] : arr[0];
+            int max1 = min2;
+            int max2 = min1;
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] < min1)
+                {
+                    min2 = min1;
+                    min1 = arr[i];
+                }
+                else if (arr[i] < min2)
+                {
+                    min2 = arr[i];
+                }
+                else if (arr[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = arr[i];
+                }
+                else if (arr[i] > max2)
+                {
+                    max2 = arr[i];
+                }
+            }
+            if (min2 < 0 && max1 < 0)
+            {
+                return max1 * max2;
+            }
+            else if (min2 <= 0 && max1 >=0)
+            {
+                return min1 * max1;
+            }
+            else 
+            {
+                return min1 * min2;
+            }
         }
     }
 }
